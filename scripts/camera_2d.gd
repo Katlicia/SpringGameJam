ee5df1b2560cf221f4d1ec2d5cda043b97487a28
Katlicia <mama-4153@hotmail.com>
2025-04-26 14:31:45 +0300

    camera

--- scripts/camera_2d.gd
+++ scripts/camera_2d.gd
@@ -3,9 +3,11 @@ extends Camera2D
 @export var target: Node2D # Takip edeceği hedef (mesela Player)
 
 var fixed_x_position = 0.0
+var res
 
 func _ready():
 	fixed_x_position = global_position.x
+	res = get_viewport().get_visible_rect().size
 
 func _process(delta):
 	if target:
